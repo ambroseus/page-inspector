a16ce221e1ee2469718c6e6d9d6163fe27995c22
ambroseus <ambroseus@gmail.com>
2020-06-23 14:36:03 +0300

    added prettier

--- server.js
+++ server.js
@@ -1,35 +1,36 @@
-const express = require('express');
-const puppeteer = require('puppeteer');
-const { ssr } = require('./ssr.js');
+const express = require('express')
+const puppeteer = require('puppeteer')
+const { ssr } = require('./ssr.js')
 
-const hostname = '127.0.0.1';
-const port = 8081;
-const host = `http://localhost:${port}`;
+const hostname = '127.0.0.1'
+const port = 8081
+const host = `http://localhost:${port}`
 
-const app = express();
+const app = express()
 
 app.listen(port, () => console.log(`page-inspector listen on ${host}`))
 
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-	next();
-});
+  res.header('Access-Control-Allow-Origin', '*')
+  next()
+})
 
-let browserWSEndpoint = null;
+let browserWSEndpoint = null
 
 app.get('/', async (req, res, next) => {
+  const { url } = req.query
 
-	const { url } = req.query;
+  if (!url) {
+    return res
+      .status(400)
+      .send(`Invalid url param: Example: ${host}/google.com`)
+  }
 
-	if (!url) {
-		return res.status(400).send(`Invalid url param: Example: ${host}/google.com`);
-	}
+  if (!browserWSEndpoint) {
+    const browser = await puppeteer.launch()
+    browserWSEndpoint = await browser.wsEndpoint()
+  }
 
-	if (!browserWSEndpoint) {
-		const browser = await puppeteer.launch();
-		browserWSEndpoint = await browser.wsEndpoint();
-	}
-
-	const { html, status } = await ssr(url, browserWSEndpoint);
-	return res.status(status).send(html);
+  const { html, status } = await ssr(url, browserWSEndpoint)
+  return res.status(status).send(html)
 })
--- ssr.js
+++ ssr.js
@@ -1,45 +1,44 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer')
 
 // https://hackernoon.com/tips-and-tricks-for-web-scraping-with-puppeteer-ed391a63d952
 // Don't download all resources, we just need the HTML
 // Also, this is huge performance/response time boost
 const blockedResourceTypes = [
-	'image',
-	'media',
-	'font',
-	'texttrack',
-	'object',
-	'beacon',
-	'csp_report',
-	'imageset',
-];
+  'image',
+  'media',
+  'font',
+  'texttrack',
+  'object',
+  'beacon',
+  'csp_report',
+  'imageset',
+]
 
 const skippedResources = [
-	'quantserve',
-	'adzerk',
-	'doubleclick',
-	'adition',
-	'exelator',
-	'sharethrough',
-	'cdn.api.twitter',
-	'google-analytics',
-	'googletagmanager',
-	'google',
-	'fontawesome',
-	'facebook',
-	'analytics',
-	'optimizely',
-	'clicktale',
-	'mixpanel',
-	'zedo',
-	'clicksor',
-	'tiqcdn',
-];
+  'quantserve',
+  'adzerk',
+  'doubleclick',
+  'adition',
+  'exelator',
+  'sharethrough',
+  'cdn.api.twitter',
+  'google-analytics',
+  'googletagmanager',
+  'google',
+  'fontawesome',
+  'facebook',
+  'analytics',
+  'optimizely',
+  'clicktale',
+  'mixpanel',
+  'zedo',
+  'clicksor',
+  'tiqcdn',
+]
 
 function inject(text) {
-	return text;
+  return text
 }
-
 
 /**
  * https://developers.google.com/web/tools/puppeteer/articles/ssr#reuseinstance
@@ -49,62 +48,59 @@ function inject(text) {
  *     a new browser instance is launched.
  */
 async function ssr(url, browserWSEndpoint) {
+  const browser = await puppeteer.connect({ browserWSEndpoint })
 
-	const browser = await puppeteer.connect({ browserWSEndpoint });
+  try {
+    const page = await browser.newPage()
+    await page.setRequestInterception(true)
 
-	try {
-		const page = await browser.newPage();
-		await page.setRequestInterception(true);
+    page.on('request', request => {
+      const requestUrl = request._url.split('?')[0].split('#')[0]
+      if (
+        blockedResourceTypes.indexOf(request.resourceType()) !== -1 ||
+        skippedResources.some(resource => requestUrl.indexOf(resource) !== -1)
+      ) {
+        request.abort()
+      } else {
+        request.continue()
+      }
+    })
 
-		page.on('request', request => {
-			const requestUrl = request._url.split('?')[0].split('#')[0];
-			if (
-				blockedResourceTypes.indexOf(request.resourceType()) !== -1 ||
-				skippedResources.some(resource => requestUrl.indexOf(resource) !== -1)
-			) {
-				request.abort();
-			} else {
-				request.continue();
-			}
-		})
+    const response = await page.goto(url, {
+      waitUntil: 'networkidle0',
+    })
 
-		const response = await page.goto(url, {
-			waitUntil: 'networkidle0'
-		});
+    await page.exposeFunction('inject', text => inject(text))
 
-		await page.exposeFunction('inject', text => inject(text));
+    // Inject <base> on page to relative resources load properly.
+    await page.evaluate(async url => {
+      const base = document.createElement('base')
+      base.href = url
+      // Add to top of head, before all other resources.
+      document.head.prepend(base)
 
-		// Inject <base> on page to relative resources load properly.
-		await page.evaluate(async (url) => {
-			const base = document.createElement('base');
-			base.href = url;
-			// Add to top of head, before all other resources.
-			document.head.prepend(base);
+      const div = document.createElement('div')
+      div.innerText = await window.inject('INJECTED CONTENT')
+      document.body.prepend(div)
+    }, url)
 
-			const div = document.createElement('div');
-      div.innerText = await window.inject('!!!!!');
-			document.body.prepend(div);
-		}, url);
+    // Remove scripts and html imports. They've already executed.
+    await page.evaluate(() => {
+      const elements = document.querySelectorAll('script, link[rel="import"]')
+      elements.forEach(e => e.remove())
+    })
 
-		// Remove scripts and html imports. They've already executed.
-		await page.evaluate(() => {
-			const elements = document.querySelectorAll('script, link[rel="import"]');
-			elements.forEach(e => e.remove());
-		});
+    const html = await page.content()
 
-		const html = await page.content();
+    // Close the page we opened here (not the browser).
+    await page.close()
 
-		// Close the page we opened here (not the browser).
-		await page.close();
+    return { html, status: response.status() }
+  } catch (e) {
+    const html = e.toString()
+    console.warn({ message: `URL: ${url} Failed with message: ${html}` })
+    return { html, status: 500 }
+  }
+}
 
-		return { html, status: response.status() }
-	}
-	catch (e) {
-		const html = e.toString();
-		console.warn({ message: `URL: ${url} Failed with message: ${html}` })
-		return { html, status: 500 }
-	}
-
-};
-
-exports.ssr = ssr;
+exports.ssr = ssr
