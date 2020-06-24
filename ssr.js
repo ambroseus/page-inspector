617ffd537a377bd96e2ff611379dc801c216c453
ambroseus <ambroseus@gmail.com>
2020-06-24 16:00:39 +0300

    get xpath to selection

--- ssr.js
+++ ssr.js
@@ -78,19 +78,47 @@ async function ssr(url, browserWSEndpoint) {
     // inject save selection button and handler
     await page.evaluate(async url => {
       function getSelection() {
-        var sel = window.getSelection()
+        let sel = window.getSelection()
         if (!sel) return false
 
-        var el = sel.focusNode
-        var path = []
-        while (el.nodeName.toLowerCase() != 'html') {
-          path.unshift(
-            el.nodeName + (el.className ? ` class='${el.className}'` : '')
-          )
-          el = el.parentNode
+        function getXPath(element) {
+          if (element.id) return `//*[@id='` + element.id + `']`
+          if (element === document.body) return element.tagName.toLowerCase()
+          let ix = 0
+          let siblings = element.parentNode.childNodes
+
+          for (let i = 0; i < siblings.length; i++) {
+            let sibling = siblings[i]
+            if (sibling === element)
+              return (
+                getXPath(element.parentNode) +
+                '/' +
+                element.tagName.toLowerCase() +
+                '[' +
+                (ix + 1) +
+                ']'
+              )
+            if (sibling.nodeType === 1 && sibling.tagName === element.tagName) {
+              ix++
+            }
+          }
         }
-        path.push(sel.toString())
-        console.log(path)
+
+        function getPath(el) {
+          let path = []
+          while (el.nodeName.toLowerCase() != 'html') {
+            path.unshift(
+              el.nodeName + (el.className ? ` class='${el.className}'` : '')
+            )
+            el = el.parentNode
+          }
+          return path
+        }
+
+        //console.log(sel)
+
+        console.log(getXPath(sel.focusNode.parentElement))
+        console.log(sel.toString())
       }
 
       const div = document.createElement('div')
