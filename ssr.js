b8a995e1c92f57ab9cc10431ca03ebaf1365ee51
ambroseus <ambroseus@gmail.com>
2020-06-24 14:35:50 +0300

    refactored

--- ssr.js
+++ ssr.js
@@ -36,16 +36,12 @@ const skippedResources = [
   'tiqcdn',
 ]
 
-function ssrlog(str) {
-  console.log(str)
-}
-
 /**
  * https://developers.google.com/web/tools/puppeteer/articles/ssr#reuseinstance
- * @param {string} url URL to prerender.
+ * @param {string} url URL to prerender
  * @param {string} browserWSEndpoint Optional remote debugging URL. If
  *     provided, Puppeteer's reconnects to the browser instance. Otherwise,
- *     a new browser instance is launched.
+ *     a new browser instance is launched
  */
 async function ssr(url, browserWSEndpoint) {
   const browser = await puppeteer.connect({ browserWSEndpoint })
@@ -72,21 +68,24 @@ async function ssr(url, browserWSEndpoint) {
 
     await page.exposeFunction('ssrlog', str => console.log(str))
 
-    // Inject <base> on page to relative resources load properly.
+    // inject <base> on page to relative resources load properly
     await page.evaluate(async url => {
       const base = document.createElement('base')
       base.href = url
-      // Add to top of head, before all other resources.
       document.head.prepend(base)
+    }, url)
 
+    // inject save selection button and handler
+    await page.evaluate(async url => {
       function getSelection() {
         var sel = window.getSelection()
         if (!sel) return false
+
         var el = sel.focusNode
         var path = []
         while (el.nodeName.toLowerCase() != 'html') {
           path.unshift(
-            el.nodeName + (el.className ? ' class=' + el.className : '')
+            el.nodeName + (el.className ? ` class='${el.className}'` : '')
           )
           el = el.parentNode
         }
@@ -94,25 +93,22 @@ async function ssr(url, browserWSEndpoint) {
         console.log(path)
       }
 
-      const getSelectionStr = getSelection.toString()
-
       const div = document.createElement('div')
       div.innerHTML = `
-				<button onClick="(${getSelectionStr})()">save selection</button>
+				<button onClick="(${getSelection.toString()})()">save selection</button>
       `
       await window.ssrlog(div.innerHTML)
+      await window.ssrlog(`url: ${url}`)
       document.body.prepend(div)
     }, url)
 
-    // Remove scripts and html imports. They've already executed.
+    // remove scripts and html imports - they've already executed
     await page.evaluate(() => {
       const elements = document.querySelectorAll('script, link[rel="import"]')
       elements.forEach(e => e.remove())
     })
 
     const html = await page.content()
-
-    // Close the page we opened here (not the browser).
     await page.close()
 
     return { html, status: response.status() }
